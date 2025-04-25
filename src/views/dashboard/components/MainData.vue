843be123900b062bbae203eeb9faeb7a1993a13b
123 <1193838563@qq.com>
2025-04-25 17:30:13 +0800

    init

--- src/views/dashboard/components/MainData.vue
+++ src/views/dashboard/components/MainData.vue
@@ -1,5 +1,52 @@
 <!-- 主要区域，地图 -->
 <script setup>
+import { onMounted, ref } from "vue";
+import * as echarts from "echarts";
+
+import nanning from "@/assets/json/nanning.json";
+
+const chartRef = ref(null);
+let chartInstance = null;
+
+const initChart = () => {
+  chartInstance = echarts.init(chartRef.value);
+  echarts.registerMap('nanning', nanning);
+  const option = {
+    // 地图背景色
+    backgroundColor: 'transparent',
+    // 地图配置
+    geo: {
+      map: 'nanning',
+      roam: false, // 不允许缩放和平移
+      // zoom: 1.2, // 缩放级别
+      label: {
+        show: true,
+        color: '#fff',
+        fontSize: 12
+      },
+      itemStyle: {
+        areaColor: '#0c3e5f', // 地图区域颜色
+        borderColor: '#4befff', // 边界颜色
+        borderWidth: 1.5,
+        shadowColor: 'rgba(75,239,255,0.3)',
+        shadowBlur: 10
+      },
+      emphasis: {
+        itemStyle: {
+          areaColor: '#4977c3', // 鼠标悬浮时的颜色
+          shadowColor: 'rgba(75,239,255,0.5)',
+          shadowBlur: 20
+        }
+      }
+    },
+    series: [] // 如果需要在地图上添加散点等数据可以在这里配置
+  };
+  chartInstance.setOption(option);
+}
+
+onMounted(() => {
+  initChart()
+})
 </script>
 
 <template>
@@ -34,6 +81,8 @@
         </div>
       </div>
     </div>
+
+    <div ref="chartRef" class="chart"></div>
   </div>
 </template>
 
@@ -62,5 +111,10 @@
 
 .total-image {
   width: 66px;
+}
+
+.chart {
+  width: 100%;
+  height: 800px;
 }
 </style>
